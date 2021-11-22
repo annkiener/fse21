91d191d06b4cfba448627442c7ee54304a5ded7c
Cacciu <m.t.c1@hotmail.com>
2021-11-22 15:02:32 +0100

    allow reading data from json file

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+let tasks = [];
+let loadedTasks  = false;
+fetch("/tasks.json").then(response =>{
+  return response.json();
+}).then( json => {
+  tasks = json;
+  loadedTasks = true;
+});
+
 // Selectors
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
 
@@ -6,24 +15,32 @@ function handleSubmitForm(e) {
     e.preventDefault();
     let input = document.querySelector("div.main input[id='fname']");
     if (input.value != '')
-        addTodo(input.value);
+        addTodoFromWebsite(input.value);
     input.value = '';
 }
 
+function addTodoFromWebsite(todo){
+    var x = document.getElementById("due").value; 
+    addTodo(todo, x, false);
+}
+
 // Helpers
-function addTodo(todo) {
+function addTodo(todo, due, done) {
     let ul = document.querySelector('ul');
     let li = document.createElement('li');
-    var x = document.getElementById("due").value;
+
     //document.getElementById("main").innerHTML = x;
     li.innerHTML = `
-        <span class="todo-item">${todo}<br><br>${x}</span>
+        <span class="todo-item">${todo}<br><br>${due}</span>
         <button name="checkButton"><i class="fas fa-square"></i></button>
         <button name="deleteButton" ><i class="fas fa-trash"></i></button>
     `;
     //li.classList.add(window.localStorage.setItem('todo', 'clean my room'));
     li.classList.add('todo-list-item');
     ul.appendChild(li);
+    if(done){
+      actuallyCheckTodo(li.children[1]); //check it
+    }
 }
 
 document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
@@ -36,18 +53,22 @@ function handleClickDeleteOrCheck(e) {
         deleteTodo(e);
 }
 
+function actuallyCheckTodo(node){
+  let item = node.parentNode;
+  if (item.style.textDecoration == 'line-through'){
+    item.style.textDecoration = 'none';
+    node.children[0].classList.replace("fa-check-square", "fa-square");
+  }
+  else{
+      item.style.textDecoration = 'line-through';
+      node.children[0].classList.replace("fa-square", "fa-check-square");
+  }
+}
+
 function checkTodo(e) {
-    let item = e.target.parentNode;
-    if (item.style.textDecoration == 'line-through'){
-        item.style.textDecoration = 'none';
-        console.log(e.target.children)
-        console.log(e.target.children[0])
-        e.target.children[0].classList.replace("fa-check-square", "fa-square");
-    }
-    else{
-        item.style.textDecoration = 'line-through';
-        e.target.children[0].classList.replace("fa-square", "fa-check-square");
-    }
+    let item = e.target;
+    console.log(item);
+    actuallyCheckTodo(item);    
 }
 
 function deleteTodo(e) {
@@ -128,5 +149,49 @@ function showResults(){
     
     answers[i].querySelector(".percentage-bar").style.width = percentage + "%";
     answers[i].querySelector(".percentage-value").innerText = percentage + "%";
+  }
+}
+
+function loadTodos(name){
+  handleClearAll(null);
+  console.log("loading todos");
+  console.log(name);
+    tasks.forEach(todo => {
+      if(todo.user == name){
+        addTodo(todo.title, todo.due, todo.done);
+      }
+    });
+}
+
+function loadingUsernames(tasklist){
+  let usernames = [];
+  tasklist.forEach(todo => {
+    if (usernames.lastIndexOf(todo.user) == -1){
+      usernames.push(todo.user);
+    }
+  });
+
+  let usernamesMarkup = document.querySelector("select");
+  usernames.forEach(username => {
+    let option = document.createElement("option");
+    option.value = username;
+    option.innerText = username;
+    usernamesMarkup.appendChild(option);
+  });
+}
+
+//handle this stupid async stuff
+function loadUsernames(){ 
+  if (!loadedTasks){
+    fetch("/tasks.json").then(response =>{
+      return response.json();
+    }).then( json => {
+      tasks = json;
+      loadedTasks = true;
+      loadingUsernames(tasks);
+    });
+  }
+  else{
+    loadingUsernames(tasks);
   }
 }
